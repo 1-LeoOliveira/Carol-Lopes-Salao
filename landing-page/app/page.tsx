cc6cadbaf299cbc8d4a51ed2525bc22376b26af4
Leonardo Oliveira <leoo2771@gmail.com>
2025-01-14 12:11:16 -0300

    remoção next imagem

--- landing-page/app/page.tsx
+++ landing-page/app/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import React, { useState, useEffect } from 'react';
-import Image from 'next/image';
 import { ChevronLeft, ChevronRight, Instagram, Facebook, Phone, Mail, Clock, MapPin } from 'lucide-react';
 
 // TypeScript interfaces
